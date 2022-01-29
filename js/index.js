d4a0311676e0b8e12293bc0f4b740968f9a025cc
Raaj <riskyrick20@gmail.com>
2022-01-29 16:22:19 +0530

    added dark mode and images and text

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
 const navToggle = document.querySelector(".mobile-nav");
 const PrimaryNav = document.querySelector(".primary-navigation");
 const navIcon = document.getElementById("primary-menu-icon");
+
+
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+const toggleTheme = () => {
+  const body = document.querySelector("body");
+  const currentTheme = body.getAttribute("data-isDark");
+  if(currentTheme === "true"){
+    darkModeToggle.innerText = 'Dark Mode';
+    body.setAttribute("data-isDark", "false");
+  }
+  else{
+    darkModeToggle.innerText = "Light Mode";
+    body.setAttribute("data-isDark", "true");    
+  }
+};
+
+darkModeToggle.addEventListener("click", toggleTheme);
+
+
 const menuFunction = () => {
   const visibility = PrimaryNav.classList.contains("open");
+
   if (visibility === false) {
     PrimaryNav.classList.add("open");
     navIcon.classList.remove("bx-menu-alt-right");
@@ -13,6 +34,7 @@ const menuFunction = () => {
     navIcon.classList.add("bx-menu-alt-right");
   }
 };
+
 navToggle.addEventListener("click", menuFunction);
 const navLinks = document.querySelectorAll(".nav__link");
 navLinks.forEach((navlink) => navlink.addEventListener("click", menuFunction));

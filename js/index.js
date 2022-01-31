0d62f70faa7f7425059192d7008c09062a602eca
Raaj <riskyrick20@gmail.com>
2022-01-31 13:17:58 +0530

    minor css and js changes

--- js/index.js
+++ js/index.js
@@ -2,24 +2,24 @@ const navToggle = document.querySelector(".mobile-nav");
 const PrimaryNav = document.querySelector(".primary-navigation");
 const navIcon = document.getElementById("primary-menu-icon");
 
-
 const darkModeToggle = document.getElementById("darkModeToggle");
+const darkModeToggleIcon = document.querySelector("#darkModeToggle i");
 
 const toggleTheme = () => {
   const body = document.querySelector("body");
   const currentTheme = body.getAttribute("data-isDark");
-  if(currentTheme === "true"){
-    darkModeToggle.innerText = 'Dark Mode';
+  if (currentTheme === "true") {
+    darkModeToggleIcon.classList.remove("bx-sun");
+    darkModeToggleIcon.classList.add("bx-moon");
     body.setAttribute("data-isDark", "false");
-  }
-  else{
-    darkModeToggle.innerText = "Light Mode";
-    body.setAttribute("data-isDark", "true");    
+  } else {
+    darkModeToggleIcon.classList.remove("bx-moon");
+    darkModeToggleIcon.classList.add("bx-sun");
+    body.setAttribute("data-isDark", "true");
   }
 };
 
 darkModeToggle.addEventListener("click", toggleTheme);
-
 
 const menuFunction = () => {
   const visibility = PrimaryNav.classList.contains("open");
